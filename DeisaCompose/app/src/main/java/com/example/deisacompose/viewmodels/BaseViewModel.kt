a96f02987660ffc900201d75e21ecab388266e51
Fav124 <favianmuhammadhafiz@gmail.com>
2026-02-25 17:33:26 +0700

    Menghapus android

--- DeisaCompose/app/src/main/java/com/example/deisacompose/viewmodels/BaseViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.deisacompose.viewmodels
-
-import androidx.lifecycle.ViewModel
-import com.example.deisacompose.data.network.ApiClient
-
-open class BaseViewModel : ViewModel() {
-    protected val apiService = ApiClient.instance
-}
